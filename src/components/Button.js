d0d02c422ea432d491e832089b85ba4b95f420c7
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-02-15 17:31:57 -0500

    Created button component and added conditional styling to it to be able to react to the different possible siuations

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,20 @@
 import React from "react";
-
 import "components/Button.scss";
+import classnames from "classnames"
 
-export default function Button(props) {
-   return <button></button>;
-}
+ export default function Button(props) {
+   const buttonClass = classnames("button", {
+     "button--confirm": props.confirm,
+     "button--danger": props.danger
+   });
+ 
+   return (
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
+   );
+ }
