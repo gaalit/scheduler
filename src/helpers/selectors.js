7d4832085af789941494a3b839172715499b6b63
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-02-17 18:50:19 -0500

    Create getAppointmentsForDay helper function and created tests for it

---
+++ src/helpers/selectors.js
@@ -0,0 +1,19 @@
+
+//function that filters api/appointments and returns an array of appointments for that specific day
+function getAppointmentsForDay(state, day) {
+ let arrAppointmentsForDay = [];
+ let filteredAppointment = [];
+
+ for(let objSpecificDay of state.days) {
+
+  if(objSpecificDay.name === day) {
+   arrAppointmentsForDay = objSpecificDay.appointments;
+   }
+  }
+  for(let appointment of arrAppointmentsForDay) {
+   filteredAppointment.push(state.appointments[appointment]);
+  }
+  return filteredAppointment;
+};
+
+export { getAppointmentsForDay};
