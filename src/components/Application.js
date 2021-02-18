14ad21f1c41c9b0b0f1d5d83a8241feec3bb7676
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-02-18 14:44:02 -0500

    Added new connection to interviewers database and refactored with getInterview selector function

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import getAppointmentsForDay from "../helpers/selectors";
+import { getAppointmentsForDay, getInterview} from "../helpers/selectors";
 
 import "components/Application.scss";
 
@@ -13,18 +13,30 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: []
+    appointments: {},
+    interviewers: {}
   });
  
 
   const setDay = day => setState({ ...state, day });
  
     useEffect(() => {
-      Promise.all([axios.get("/api/days"), axios.get("/api/appointments")])
+      Promise.all([axios.get("/api/days"), axios.get("/api/appointments"), axios.get("/api/interviewers")])
       .then(all => {
-        setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}))
+        setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
       })
     }, [])
+
+    const schedule = getAppointmentsForDay(state, state.day).map(appointment => { 
+      const interview = getInterview(state, appointment.interview)
+      return (
+       <Appointment key={appointment.id}
+       id={appointment.id}
+       time={appointment.time}
+       interview={interview}/>
+      );
+    });
+    
 
   return (
     <main className="layout">
@@ -43,7 +55,7 @@ export default function Application(props) {
 />
       </section>
       <section className="schedule">
-        {getAppointmentsForDay(state, state.day).map(appointment => <Appointment key={appointment.id} {...appointment}/>)}
+       {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
