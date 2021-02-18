73353b41c40b4d620d4de17020b53dcad99ba0df
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-02-18 14:41:14 -0500

    Added getInterview selector and added 2 tests for getInterview function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,6 +1,6 @@
 
 //function that filters api/appointments and returns an array of appointments for that specific day
-export default function getAppointmentsForDay(state, day) {
+function getAppointmentsForDay(state, day) {
  let arrAppointmentsForDay = [];
  let filteredAppointment = [];
 
@@ -16,3 +16,32 @@ export default function getAppointmentsForDay(state, day) {
   return filteredAppointment;
 };
 
+//The function should return a new object containing the interview data when we pass it an object that contains the interviewer.
+                  //state.appointments["3"].interview)
+function getInterview(state, interview) {
+
+  if(interview === null) {
+    return null;
+  }
+
+return {...interview, interviewer: state.interviewers[interview.interviewer]}
+
+   
+  }
+
+
+// {  
+//   "student": "Lydia Miller-Jones",
+//   "interviewer": {  
+//     "id": 1,
+//     "name": "Sylvia Palmer",
+//     "avatar": "https://i.imgur.com/LpaY82x.png"
+//   }
+// }
+
+//interview: { student: "Chad Takahashi", interviewer: 2 }
+
+//have to add the interviewers info into the interview key
+
+
+export {getAppointmentsForDay, getInterview};
