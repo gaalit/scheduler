07298c4427a62ffd3a3b14587c56d950ac6c9e9f
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-02-17 19:34:23 -0500

    Create a State Object to manage all the states more efficiently

--- src/components/Application.js
+++ src/components/Application.js
@@ -47,10 +47,18 @@ const appointments = [
   }
 ];
 
+
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days}));
 
   useEffect(() => {
     axios
@@ -67,7 +75,7 @@ export default function Application(props) {
   alt="Interview Scheduler"
 />
 <hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu"><DayList days={days} day={day} setDay={setDay}/></nav>
+<nav className="sidebar__menu"><DayList days={state.days} day={state.day} setDay={setDay}/></nav>
 <img
   className="sidebar__lhl sidebar--centered"
   src="images/lhl.png"
