15ad5a1da4f77ecd272612cf286a733d1bbcb4b9
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-02-22 14:58:52 -0500

    Added propType test to verify interviewList is an array

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,27 +1,27 @@
 import React from "react";
 import "components/InterviewerList.scss";
-import InterviewerListItem from "./InterviewerListItem"
-
+import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from "prop-types";
 
 export default function InterviewerList(props) {
-
-
-    return(
-      <section className="interviewers">
+  return (
+    <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
-        {props.interviewers.map(interviewer => (
-          <InterviewerListItem 
-          key={interviewer.id}
-          name={interviewer.name}
-          avatar={interviewer.avatar}
-          selected={interviewer.id === props.interviewer}
-          setInterviewer={event => props.setInterviewer(interviewer.id)}
-        /> 
-        )
-      )}
-    
-     </ul>
+        {props.interviewers.map((interviewer) => (
+          <InterviewerListItem
+            key={interviewer.id}
+            name={interviewer.name}
+            avatar={interviewer.avatar}
+            selected={interviewer.id === props.interviewer}
+            setInterviewer={(event) => props.setInterviewer(interviewer.id)}
+          />
+        ))}
+      </ul>
     </section>
-    ) 
+  );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+};
