aa6554a5a9a16b15795ea108a660a48e259bbc5d
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-02-23 17:30:55 -0500

    Added mock data to be able to test axios.put requests that are made to the app

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -79,4 +79,13 @@ export default {
       });
     }
   }),
+  put: jest.fn((url) => {
+    if (url === "/api/appointments/1") {
+      console.log("MOCK PUT");
+      return Promise.resolve({
+        status: 204,
+        statusText: "Test",
+      });
+    }
+  }),
 };
