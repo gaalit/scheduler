feff551be045b40121d74531389c2c19cde9090d
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-02-23 17:32:31 -0500

    Added a data-test id to be able to test the list in the DayListItem compoenent

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,26 +1,29 @@
 import React from "react";
 import "components/DayListItem.scss";
-import classnames from "classnames"
+import classnames from "classnames";
 
 export default function DayListItem(props) {
-
-  const dayListItemClass = classnames( "day-list__item", {
-    "day-list__item--selected" : props.selected,
-    "day-list__item--full" : props.spots === 0
-  })
+  const dayListItemClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
 
   const formatSpots = (numberOfSpots) => {
-    if(numberOfSpots === 0) {
-      return `no spots remaining`
+    if (numberOfSpots === 0) {
+      return `no spots remaining`;
     } else if (numberOfSpots === 1) {
-      return `${numberOfSpots} spot remaining`
+      return `${numberOfSpots} spot remaining`;
     } else {
-      return `${numberOfSpots} spots remaining`
+      return `${numberOfSpots} spots remaining`;
     }
-  }
+  };
 
   return (
-    <li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
+    <li
+      data-testid="day"
+      className={dayListItemClass}
+      onClick={() => props.setDay(props.name)}
+    >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
